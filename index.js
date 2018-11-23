dd680855294d0fe383f3758e44f19c15b7356c8a
Peter Keller <petekeller2@gmail.com>
2018-11-23 09:44:34 -0500

    Make useable version

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = ["Test"];
+module.exports = {
+  getStep: function () {
+    return [[this.testMethodByTest, []], [[this.testMethodTwo, []]]];
+  },
+  testMethodByTest: function () {
+    console.log('Test method');
+  },
+  testMethodTwo: function () {
+    console.log('Test method two');
+  },
+  filesByTestReduce: function (filesInTempDir, extraArgs, specificFunc) {
+    filesInTempDir.forEach(function(element) {
+      specificFunc();
+      console.log('testy', element);
+    });
+    return filesInTempDir;
+  },
+};
