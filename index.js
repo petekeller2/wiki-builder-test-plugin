04214d50dd135c1aba205620c330550498eb8d46
Peter Keller <petekeller2@gmail.com>
2019-06-15 13:19:29 -0400

    Writing to log file

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const fs = require('fs');
+
 module.exports = {
   getStep: function () {
     return [[this.testMethodByTest, []], [[this.testMethodTwo, []]]];
   },
   testMethodByTest: function () {
     console.log('Test method');
+    fs.appendFile('log.txt', 'testMethodByTest was called\n', function (err) {
+      if (err) throw err;
+      console.log('Write for testMethodByTest Saved!');
+    });
   },
   testMethodTwo: function () {
     console.log('Test method two');
+    fs.appendFile('log.txt', 'testMethodTwo was called\n', function (err) {
+      if (err) throw err;
+      console.log('Write for testMethodTwo Saved!');
+    });
   },
   filesByTestReduce: function (filesInTempDir, extraArgs, specificFunc) {
     filesInTempDir.forEach(function(element) {
